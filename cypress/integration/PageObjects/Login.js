dee17e659bc5948d4477a9731033d1bda286f722
Daniel Ramirez <daniel_ramriez82151@elpoli.edu.co>
2020-11-04 17:26:37 -0500

    First page class added

---
+++ cypress/integration/PageObjects/Login.js
@@ -0,0 +1,14 @@
+class LoginPage {
+
+    constructor() {
+        this.usernameInput = '#txtUsername';
+        this.passwordInput = 'txtPassword';
+        this.loginButton = 'btnLogin';
+    }
+
+    login = (username, password) => {
+        cy.get(this.usernameInput).type(username);
+        cy.get(this.passwordInput).type(password);
+        cy.get(this.loginButton).click();
+    }
+}
